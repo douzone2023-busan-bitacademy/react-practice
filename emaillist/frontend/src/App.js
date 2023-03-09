9acf0fe2c4effc5f1875f01d43d44b7c1a9fec11
douzone2023-busan-bitacademy <121003218+douzone2023-busan-bitacademy@users.noreply.github.com>
2023-03-09 18:10:02 +0900

    Add files via upload

--- emaillist/frontend/src/App.js
+++ emaillist/frontend/src/App.js
@@ -1,11 +1,110 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import RegisterForm from './RegisterForm';
+import SearchBar from './SearchBar';
+import Emaillist from './Emaillist';
+import './assets/scss/App.scss';
 
-function App(props) {
-    return (
-        <div id='App'>            
-           <h1>Emaillist</h1>
-        </div>
-    );
+const App = () => {
+  const [emails, setEmails] = useState([]);
+
+  const emailDelete = function(no) {
+    console.log(no);
+  }
+
+  const emailAdd = async function(email) {
+    try {
+      const response = await fetch(`/api`, {
+        method: 'post',
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+        },
+        body: JSON.stringify(email)
+      });
+
+      if(!response.ok) {
+        throw new Error(`${response.status} ${response.statusText}`);
+      }
+
+      const json = await response.json();
+
+      if(json.result !== 'success') {
+        throw new Error(`${json.result} ${json.message}`);  
+      }
+
+      setEmails([json.data, ...emails]);
+    } catch(err) {
+      console.log(err);
+    }
+  }
+
+  const keywordChanged = async function(keyword) {
+    try {
+        const response = await fetch(`/api?kw=${keyword}`, {
+          method: 'get',
+          headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+          },
+          body: null
+        });
+
+        if(!response.ok) {
+          throw new Error(`${response.status} ${response.statusText}`);
+        }
+
+        const json = await response.json();
+
+        if(json.result !== 'success') {
+          throw new Error(`${json.result} ${json.message}`);  
+        }
+
+        setEmails(json.data);
+      } catch(err) {
+        console.log(err);
+      }
+  }
+
+  
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('/api', {
+          method: 'get',
+          headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+          },
+          body: null
+        });
+
+        if(!response.ok) {
+          throw new Error(`${response.status} ${response.statusText}`);
+        }
+
+        const json = await response.json();
+
+        if(json.result !== 'success') {
+          throw new Error(`${json.result} ${json.message}`);  
+        }
+
+        setEmails(json.data);
+      } catch(err) {
+        console.log(err);
+      }
+    }
+
+    fetchData();
+    
+  }, []);
+
+  return (
+    <div id={'App'}>
+      <RegisterForm callback={emailAdd} />
+      <SearchBar callback={keywordChanged} />
+      <Emaillist emails={emails} />
+    </div>
+  )
 }
 
-export default App;
+export default App
