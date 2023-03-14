904528e4759d3100078b8707a430808338c57e29
douzone2023-busan-bitacademy <douzone2023.busan.bitacademy@gmail.com>
2023-03-14 11:15:28 +0900

    clock #1

---
+++ 2.component/ex05/src/04/App.js
@@ -0,0 +1,24 @@
+import React, {useState, useEffect} from 'react';
+import './assets/scss/App.scss'
+import Clock from './Clock';
+
+export default function App() {
+    const [ticks, setTicks] = useState(0);
+
+    return (
+        <div>
+            <span>{ticks}</span>
+            {
+                ticks % 10 === 0 ?
+                null :
+                <Clock
+                    message={'ex05: useEffect Hook example'}
+                    hours={'10'}
+                    minutes={'20'}
+                    seconds={'03'}/>
+            }
+        </div>
+    );
+
+
+}
