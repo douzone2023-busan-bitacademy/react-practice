0d316d475bf82723343750226078a998ba3e6370
douzone2023-busan-bitacademy <121003218+douzone2023-busan-bitacademy@users.noreply.github.com>
2023-03-15 03:18:04 +0900

    Update App.js

--- 2.component/ex05/src/04/App.js
+++ 2.component/ex05/src/04/App.js
@@ -3,22 +3,48 @@ import './assets/scss/App.scss'
 import Clock from './Clock';
 
 export default function App() {
+    console.log("!!!");
+    const getCurrentClockTime = () => {
+        const now = new Date();
+        const hours = now.getHours();
+
+        return {
+            hours: ('0' + (hours > 12 ? hours - 12 : hours)).slice(-2),
+            minutes: ('0' + now.getMinutes()).slice(-2),
+            seconds: ('0' + now.getSeconds()).slice(-2),
+            session: hours > 12 ? 'pm': 'am'
+        }
+    }
+
+    const [currentTime, setCurrentTime] = useState(getCurrentClockTime());
     const [ticks, setTicks] = useState(0);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setCurrentTime(getCurrentClockTime());
+            setTicks(t => t + 1);  
+        }, 1000);
+
+        return (() => {
+            clearInterval(interval);
+        });       
+    }, []);
+
+    // useEffect(() => {
+    //     setTimeout(() => {
+    //         setCurrentTime(getCurrentClockTime());
+    //         setTicks(ticks+1);  
+    //     }, 1000);
+    // }, [currentTime]);
 
     return (
         <div>
             <span>{ticks}</span>
-            {
-                ticks % 10 === 1 ?
-                null :
-                <Clock
-                    message={'ex05: useEffect Hook example'}
-                    hours={'10'}
-                    minutes={'20'}
-                    seconds={'03'}/>
-            }
-        </div>
+            <Clock
+                message={'ex05: useEffect Hook example'}
+                hours={currentTime.hours}
+                minutes={currentTime.minutes}
+                seconds={currentTime.seconds}/>
+        </div>        
     );
-
-
 }
